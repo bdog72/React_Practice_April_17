c9d59fc78ec761a51861de3a5ed2b41389d8807c
Brian Bycynski <high5bri@hotmail.com>
2017-04-15 07:41:09 -0400

    installed react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import App1 from './App1'
+import App2 from './App2'
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
-      <App1 />
-    </div>
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <App1 />
+        <br />
+        <App2 />
+      </div>
+    )
   }
 }
 
