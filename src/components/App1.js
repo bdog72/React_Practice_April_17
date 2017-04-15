c4f92bae5c2e8e18bda3d782b0a5ebf7c8252445
Brian Bycynski <high5bri@hotmail.com>
2017-04-15 06:13:14 -0400

    centered image

--- src/components/App1.js
+++ src/components/App1.js
@@ -3,7 +3,7 @@ import { Image } from 'react-bootstrap'
 export default class App1 extends Component {
   render () {
     return <div>
-      <Image src='https://vetstreet.brightspotcdn.com/dims4/default/ed822f6/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F75%2F371580ca7a11e0ad9e12313817c323%2Ffile%2FBeagle-1-645mk062311.jpg' alt='' responsive />
+      <Image className='beagle1'src='https://vetstreet.brightspotcdn.com/dims4/default/ed822f6/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F75%2F371580ca7a11e0ad9e12313817c323%2Ffile%2FBeagle-1-645mk062311.jpg' alt='' responsive />
     </div>
   }
 }
