44955761a10f208eb7b7b02c9e35a91a6fb611bd
Brian Bycynski <high5bri@hotmail.com>
2017-04-16 06:00:45 -0400

    started 1st example

--- src/components/App2.js
+++ src/components/App2.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react'
 import { Button, ButtonToolbar } from 'react-bootstrap'
 export default class App2 extends Component {
   render () {
+    const ex1 = <h2>Bdog you the man</h2>
+    const But = (
+      <a href='https://www.yahoo.com'><ButtonToolbar>
+        <Button bsStyle='primary'>Primary</Button>
+        <Button bsStyle='danger'>Danger</Button>
+      </ButtonToolbar></a>
+    )
     return (
-      <div>
-        { ex1 }
-        <br />
-        { But }
+      <div className='app2'>
+        {ex1}
+        {But}
       </div>
     )
   }
 }
-const ex1 = <h2>Bdog you the man</h2>
-const But = <ButtonToolbar>
-  <Button bsStyle='primary'>Primary</Button>
-  <Button bsStyle='danger'>Danger</Button>
-</ButtonToolbar>
